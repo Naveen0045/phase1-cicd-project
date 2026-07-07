9f06ba81d0754dc6818306ea5f46be19bde07f09
Ubuntu <naveenthangaraj24@gmail.com>
2026-07-07 08:52:13 +0000

    Version 3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from GitLab CI/CD on AWS EC2"
+    return "Version 3 - CI/CD Working successfully"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
